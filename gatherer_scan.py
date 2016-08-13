e89e5b22d9687e0749da033a819880c7fce2fea3
wanderso <williamharrisanderson@gmail.com>
2016-08-12 21:32:49 -0700

    Scraper online

--- gatherer_scan.py
+++ gatherer_scan.py
@@ -2,33 +2,35 @@ import requests
 import bs4
 
 
-r = requests.get(\
-    'http://gatherer.wizards.com/Pages/Search/Default.aspx?page=0&set=%5B%22Shadows%20over%20Innistrad%22%5D')
+def scrape_soi():
 
-with open("soi.txt", "w") as f:
-    f.write(r.text.encode('utf-8'))
+    r = requests.get(\
+        'http://gatherer.wizards.com/Pages/Search/Default.aspx?page=0&set=%5B%22Shadows%20over%20Innistrad%22%5D')
 
-r = requests.get(\
-    'http://gatherer.wizards.com/Pages/Search/Default.aspx?page=1&set=%5B%22Shadows%20over%20Innistrad%22%5D')
+    with open("soi.txt", "w") as f:
+        f.write(r.text.encode('utf-8'))
 
-with open("soi.txt", "a") as f:
-    f.write(r.text.encode('utf-8'))
+    r = requests.get(\
+        'http://gatherer.wizards.com/Pages/Search/Default.aspx?page=1&set=%5B%22Shadows%20over%20Innistrad%22%5D')
 
-r = requests.get(\
-    'http://gatherer.wizards.com/Pages/Search/Default.aspx?page=2&set=%5B%22Shadows%20over%20Innistrad%22%5D')
+    with open("soi.txt", "a") as f:
+        f.write(r.text.encode('utf-8'))
 
-with open("soi.txt", "a") as f:
-    f.write(r.text.encode('utf-8'))
+    r = requests.get(\
+        'http://gatherer.wizards.com/Pages/Search/Default.aspx?page=2&set=%5B%22Shadows%20over%20Innistrad%22%5D')
 
-r = requests.get(\
-    'http://gatherer.wizards.com/Pages/Search/Default.aspx?page=3&set=%5B%22Shadows%20over%20Innistrad%22%5D')
+    with open("soi.txt", "a") as f:
+        f.write(r.text.encode('utf-8'))
 
-with open("soi.txt", "a") as f:
-    f.write(r.text.encode('utf-8'))
+    r = requests.get(\
+        'http://gatherer.wizards.com/Pages/Search/Default.aspx?page=3&set=%5B%22Shadows%20over%20Innistrad%22%5D')
 
-#print (r.text)
+    with open("soi.txt", "a") as f:
+        f.write(r.text.encode('utf-8'))
 
-soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    #print (r.text)
+
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
 
 
 #print(soup.find_all('tr'))
